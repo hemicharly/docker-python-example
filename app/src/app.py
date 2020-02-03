087f4771af6f19aa3e2d9aa2d65e0c71e1fc48c2
hemicharly <hemicharlythiago@gmail.com>
2020-02-03 20:53:27 -0300

    feat: added register logs app

--- app/src/app.py
+++ app/src/app.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,9 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    app.logger.warning('Hello World')
+    return 'Hello World'
 
 
 if __name__ == '__main__':
-    print("Application started...")
+    handler = RotatingFileHandler('/logs/app.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run(debug=True, host="0.0.0.0", port=8000)
